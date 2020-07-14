d0a4d627a13b21564220cabf0c01f7ab4c2cd03e
DoongE <whdlwns20@gmail.com>
2020-07-14 17:52:08 +0900

    Facade Pattern 적용중

---
+++ MakeImageFromYoutube/Facade/Functions/YoutubeVideo.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeImageFromYoutube.Facade.Functions
+{
+    class YoutubeVideo
+    {
+        public int Download(string exePath, string saveVideoName, string youtubeURL)
+        {
+            try
+            {
+                // 경로 저장
+                //string exePath = Application.StartupPath + @"\ffmpeg\bin\youtube-dl";
+                //string savedVideoName = storageLocationTextBox.Text + @"\" + savedVideoFileNameTextBox.Text;
+                //string command = exePath + " -o " + savedVideoName + " " + youtubeURLTextBox.Text;
+                string command = exePath + " -o " + saveVideoName + " -f bestvideo+bestaudio --merge-output-format mkv " + youtubeURL;
+
+                ProcessStartInfo pri = new ProcessStartInfo();
+                Process pro = new Process();
+
+                // 실행할 파일명 입력
+                pri.FileName = "cmd.exe";
+
+                // cmd 창 띄우기
+                pri.CreateNoWindow = false; // false -> 띄우기, true -> 안 띄우기
+                pri.UseShellExecute = false;
+
+                pri.RedirectStandardInput = true;
+                pri.RedirectStandardOutput = true;
+                pri.RedirectStandardError = true;
+
+                pro.StartInfo = pri;
+                pro.Start();
+
+                // 명령어 실행
+                pro.StandardInput.Write(command + Environment.NewLine);
+                pro.StandardInput.Close();
+
+                // 이 코드가 있어야 제대로 유튜브 영상을 다운받을 수 있다.
+                string result = pro.StandardOutput.ReadToEnd();
+
+                // 다운로드 로그 저장
+                //log.WriteLog(result);
+
+                pro.WaitForExit();
+                pro.Close();
+
+                return 1;
+            }
+            catch (Exception ex)
+            {
+                //log.WriteLog("[ERROR] : " + ex);
+                return 0;
+            }
+        }
+    }
+}
---
+++ MakeImageFromYoutube/Facade/YoutubeToImages.cs
@@ -0,0 +1,32 @@
+﻿using MakeImageFromYoutube.Facade.Functions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeImageFromYoutube.Facade
+{
+    class YoutubeToImages
+    {
+        #region # Property
+
+        // 사용할 기능(클래스) 전역변수로 선언
+        private YoutubeVideo youtubeVideo;
+        private YoutubeAudio youtubeAudio;
+        private ToImages toImages;
+
+        #endregion
+
+        #region # Constructor
+
+        public YoutubeToImages()
+        {
+            youtubeVideo = new YoutubeVideo();
+            youtubeAudio = new YoutubeAudio();
+            toImages = new ToImages();
+        }
+
+        #endregion
+    }
+}
